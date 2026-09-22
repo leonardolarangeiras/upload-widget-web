698c17eb346eb1cbb2d0ea039f6a4a2ab2a144da
leonardolarangeiras <leonardoslarangeiras@gmail.com>
2026-09-22 16:04:35 -0300

    feat: add dropzone, title and widget structure

--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -1,19 +1,33 @@
+import { useState } from "react";
+
+import * as Collapsible from "@radix-ui/react-collapsible";
+
 import { UploadWidgetDropzone } from "./upload-widget-dropzone";
 import { UploadWidgetHeader } from "./upload-widget-header";
 import { UploadWidgetUploadList } from "./upload-widget-upload-list";
+import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button";
 
 export function UploadWidget() {
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+
   return (
-    <div className="bg-zinc-900 w-full overflow-hidden max-w-[360px] rounded-xl shadow-shape">
-      <UploadWidgetHeader />
+    <Collapsible.Root onOpenChange={setIsWidgetOpen}>
+      <div className="bg-zinc-900  overflow-hidden w-[360px] rounded-xl shadow-shape">
+        {!isWidgetOpen && <UploadWidgetMinimizedButton />}
 
-      <div className="flex flex-col gap-4 py-3">
-        <UploadWidgetDropzone />
+        <Collapsible.Content>
 
-        <div className="h-px bg-zinc-800 border-t border-black/50 box-content" />
+          <UploadWidgetHeader />
 
-        <UploadWidgetUploadList />
+          <div className="flex flex-col gap-4 py-3">
+            <UploadWidgetDropzone />
+
+            <div className="h-px bg-zinc-800 border-t border-black/50 box-content" />
+
+            <UploadWidgetUploadList />
+          </div>
+        </Collapsible.Content>
       </div>
-    </div>
+    </Collapsible.Root >
   );
 }
