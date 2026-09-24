f4462fe1d041ab85e66385f1d2e3911c59108078
leonardolarangeiras <leonardoslarangeiras@gmail.com>
2026-09-24 11:22:43 -0300

    feat: add and implement zustand for state management

--- src/components/upload-widget-upload-list.tsx
+++ src/components/upload-widget-upload-list.tsx
@@ -1,7 +1,10 @@
 import { UploadWidgetUploadItem } from "./upload-widget-upload-item";
+import { useUploads } from "../store/uploads";
 
 export function UploadWidgetUploadList() {
-  const isUploadListEmpty = false;
+  const {uploads} = useUploads();
+
+  const isUploadListEmpty = uploads.size === 0;
 
   return (
     <div className="px-3 flex flex-col gap-3">
@@ -13,8 +16,9 @@ export function UploadWidgetUploadList() {
         <span className="text-xs text-zinc-400">No uploads added</span>
       ) : (
         <div className="flex flex-col gap-2">
-          <UploadWidgetUploadItem />
-          <UploadWidgetUploadItem />
+          {Array.from(uploads).map(([uploadId, upload]) => (
+            <UploadWidgetUploadItem key={uploadId} upload={upload} />
+          ))}
         </div>
       )}
     </div>
