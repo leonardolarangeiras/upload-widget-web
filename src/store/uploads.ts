6eb6b3394c423875d68cd18a3c4853f7ed0b7647
leonardolarangeiras <leonardoslarangeiras@gmail.com>
2026-09-24 11:41:10 -0300

    feat: add immer to improve array set

--- src/store/uploads.ts
+++ src/store/uploads.ts
@@ -1,35 +1,39 @@
 import { create } from "zustand";
+import { enableMapSet } from "immer";
+import { immer } from "zustand/middleware/immer";
+
+enableMapSet();
 
 export type Upload = {
-  name: string;
-  file: File;
+    name: string;
+    file: File;
 };
 
 type UploadState = {
-  uploads: Map<string, Upload>;
-  addUploads: (files: File[]) => void;
+    uploads: Map<string, Upload>;
+    addUploads: (files: File[]) => void;
 };
 
-export const useUploads = create<UploadState>((set, get) => {
-  function addUploads(files: File[]) {
-    for (const file of files) {
-      const uploadId = crypto.randomUUID();
+export const useUploads = create<UploadState, [["zustand/immer", never]]>(
+    immer((set) => {
+        function addUploads(files: File[]) {
+            for (const file of files) {
+                const uploadId = crypto.randomUUID();
 
-      const upload: Upload = {
-        name: file.name,
-        file,
-      };
+                const upload: Upload = {
+                    name: file.name,
+                    file,
+                };
 
-      set((state) => {
+                set((state) => {
+                    state.uploads.set(uploadId, upload);
+                });
+            }
+        }
+
         return {
-          uploads: state.uploads.set(uploadId, upload),
+            uploads: new Map(),
+            addUploads,
         };
-      });
-    }
-  }
-
-  return {
-    uploads: new Map(),
-    addUploads,
-  };
-});
+    })
+);
